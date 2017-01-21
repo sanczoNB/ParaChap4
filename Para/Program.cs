177bec2b421317f7dcecf5efa6f722479424b575
sanczoNB <sanczo16@gmail.com>
2017-01-21 15:32:50 +0100

    Przykład z parą przechowująca typ int i double

--- Para/Program.cs
+++ Para/Program.cs
@@ -6,70 +6,26 @@ namespace Para
     {
         static void Main(string[] args)
         {
-            var random = new Random();
+            var r = new Random();
 
-            //int
-            var pi = new Para<int>[10];
-            for (var i = 0; i < pi.Length; i++)
+            //int-double
+            var pairs = new Para<int, double>[10];
+            for (int i = 0; i < pairs.Length; i++)
             {
-                pi[i] = new Para<int>(random.Next(10), random.Next(10));
+                pairs[i] = new Para<int, double>(r.Next(10), r.NextDouble());
             }
-            Console.WriteLine("Para<int>:");
-            foreach (var para in pi)
+            Console.WriteLine("Para<int, double>:");
+            foreach (var pair in pairs)
             {
-                Console.WriteLine(para);
+                Console.WriteLine(pair);
             }
-            Array.Sort(pi);
-            Console.WriteLine("\nPara<int> po sortowaniu:");
-            foreach (var para in pi)
+            Array.Sort(pairs);
+            Console.WriteLine("\nPara<int, double> po sortowaniu:");
+            foreach (var pair in pairs)
             {
-                Console.WriteLine(para);                
-            }
-
-            
-            //double
-            var pd = new Para<double>[10];
-            for (var i = 0; i < pd.Length; i++)
-            {
-                pd[i] = new Para<double>(random.Next(), random.Next());
-            }
-            Console.WriteLine("\nPara<double>:");
-            foreach (var para in pd)
-            {
-                Console.WriteLine(para);
-            }
-            Array.Sort(pd);
-            Console.WriteLine("\nPara<double> po sortowaniu:");
-            foreach (var para in pd)
-            {
-                Console.WriteLine(para);
+                Console.WriteLine(pair);
             }
 
-            //string
-            var ps = new Para<string>[12];
-            ps[0] = new Para<string>("Bester", "Alfred");
-            ps[1] = new Para<string>("Dick", "Philip");
-            ps[2] = new Para<string>("Tolkien", "John");
-            ps[3] = new Para<string>("Lem", "Stanisław");
-            ps[4] = new Para<string>("Anderson", "Poul");
-            ps[5] = new Para<string>("Pohl", "Frederik");
-            ps[6] = new Para<string>("Le Guin", "Ursula");
-            ps[7] = new Para<string>("Card", "Orson");
-            ps[8] = new Para<string>("Gibson", "William");
-            ps[9] = new Para<string>("Asimov", "Isaac");
-            ps[10] = new Para<string>("Niven", "Larry");
-            ps[11] = new Para<string>("Herbert", "Frank");
-            Console.WriteLine("\nPara<string>:");
-            foreach (var para in ps)
-            {
-                Console.WriteLine(para);
-            }
-            Array.Sort(ps);
-            Console.WriteLine("\nPara<string> po sortowaniu:");
-            foreach (var para in ps)
-            {
-                Console.WriteLine(para);
-            }
             Console.ReadKey();
         }
     }
