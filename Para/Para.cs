6e5f655691957fb6b948cd615b9510cdd8825311
sanczoNB <sanczo16@gmail.com>
2017-01-21 15:14:50 +0100

    Para jest może przechowywać dwa różne typy

--- Para/Para.cs
+++ Para/Para.cs
@@ -28,4 +28,33 @@ namespace Para
             return $"{_first}\t{_second}";
         }
     }
+
+    public class Para<T, C> : IComparable<Para<T,C>> 
+        where T : IComparable<T>
+        where C : IComparable<C>
+    {
+        private T _first = default(T);
+        private C _second = default(C);
+
+        public Para(T first, C second)
+        {
+            _first = first;
+            _second = second;
+        }
+
+        public int CompareTo(Para<T,C> other)
+        {
+            var comparisonFirstElem = _first.CompareTo(other._first);
+            if (comparisonFirstElem == 0)
+            {
+                return _second.CompareTo(other._second);
+            }
+            return comparisonFirstElem;
+        }
+
+        public override string ToString()
+        {
+            return $"{_first}\t{_second}";
+        }
+    }
 }
